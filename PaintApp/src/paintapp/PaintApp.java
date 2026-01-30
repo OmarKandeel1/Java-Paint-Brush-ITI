30994fb3f5dc45c08d7a28f3c8d0d07d257a8209
shadygamaall <sm561042@gmail.com>
2026-01-30 19:09:38 +0200

    Initial project structure

---
+++ PaintApp/src/paintapp/PaintApp.java
@@ -0,0 +1,18 @@
+
+package paintapp;
+
+/**
+ *
+ * @author Omar Mamdouh Kandeel
+ * @author Reem Adel
+ */
+public class PaintApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
